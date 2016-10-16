09c7f3070c8a5021ea0e9da9c6d392b1a5d98eb7
sharafian <ben@sharafian.me>
2016-10-16 16:15:53 -0700

    add reduce and initial value

--- jpipe.js
+++ jpipe.js
@@ -10,8 +10,18 @@ process.on('uncaughtException', (err) => {
 
 program
   .usage('[options] <expression>')
+  .description(
+      'A stream editor in javascript. Evaluates <expression> for each\n'
+    + '  line of STDIN, where:\n\n'
+    + '      _ = the current line\n'
+    + '  _line = the current line number (starting at 0)\n'
+    + '  _prev = the previous result of the expression\n'
+    + '          (starts as \'\' or as [value] if --initial is set)\n\n'
+    + '  chalk = the \'chalk\' module.')
   .version('1.0.0')
 //  .option('-r, --require [module]', 'require a module')
+  .option('-r, --reduce', 'print only the last value computed')
+  .option('-i, --initial [value]', 'set the inital value of "_prev"')
   .option('-s, --swallow', 'swallow errors')
   .option('-t, --trim', 'trim whitespace from output')
   .option('-n, --nonewline', 'don\'t add newline (as `echo -n`)')
@@ -31,6 +41,8 @@ const pipe = readline.createInterface({
 })
 
 let lineNumber = 0
+let previous = program.initial || ''
+
 pipe.on('line', (line) => {
   let out
 
@@ -39,6 +51,7 @@ pipe.on('line', (line) => {
 
       '_': line, // current line
       '_line': lineNumber++, // current line number
+      '_prev': previous, // aggregator
 
       'chalk': chalk // include the chalk module
 
@@ -49,9 +62,20 @@ pipe.on('line', (line) => {
   }
 
   if (out !== 0 && !out && !program.falsey) return
+  previous = out // only set previous if output was truthy
+  
+  if (program.reduce) return
   if (program.trim) out = out.trim()
 
   process.stdout.write('' + out)
 
   if (!program.nonewline) process.stdout.write('\n')
 })
+
+// print the last result if --reduce is on
+if (program.reduce) {
+  pipe.on('close', () => {
+    process.stdout.write('' + previous)
+    if (!program.nonewline) process.stdout.write('\n')
+  })
+}
